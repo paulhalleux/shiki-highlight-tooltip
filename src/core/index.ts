bf872becc75982ae7f37b9a75cb13619daac5f2a
paulhalleux <paulhalleux21@gmail.com>
2025-01-12 20:28:47 +0100

    refactor: clean code

--- src/core/index.ts
+++ src/core/index.ts
@@ -33,42 +33,47 @@ export function createTooltipTransformer(
     span(node) {
       for (const { regex, title, content } of configs) {
         const firstChild = node.children && node.children[0];
-        if (firstChild && firstChild.type === "text") {
-          const match = regex.exec(firstChild.value);
-          if (match) {
-            const matchIndex = match.index;
-            const matchLength = match[0].length;
-
-            // Split the original text into parts
-            const beforeMatch = firstChild.value.slice(0, matchIndex);
-            const matchedText = firstChild.value.slice(
-              matchIndex,
-              matchIndex + matchLength,
-            );
-            const afterMatch = firstChild.value.slice(matchIndex + matchLength);
-
-            return {
-              ...node,
-              children: [
-                h("span", beforeMatch),
-                {
-                  type: "element",
-                  tagName: "span",
-                  properties: {
-                    className: "tooltip-trigger",
-                  },
-                  children: [{ type: "text", value: matchedText }],
-                  data: {
-                    _type: "tooltip-trigger",
-                    title,
-                    content,
-                  },
-                },
-                h("span", afterMatch),
-              ],
-            };
-          }
+        if (!(firstChild && firstChild.type === "text")) {
+          continue;
         }
+
+        const match = regex.exec(firstChild.value);
+        if (!match) {
+          continue;
+        }
+
+        const matchIndex = match.index;
+        const matchLength = match[0].length;
+
+        // Split the original text into parts
+        const beforeMatch = firstChild.value.slice(0, matchIndex);
+        const afterMatch = firstChild.value.slice(matchIndex + matchLength);
+        const matchedText = firstChild.value.slice(
+          matchIndex,
+          matchIndex + matchLength,
+        );
+
+        return {
+          ...node,
+          children: [
+            h("span", beforeMatch),
+            {
+              ...h(
+                "span",
+                {
+                  className: "tooltip-trigger",
+                },
+                matchedText,
+              ),
+              data: {
+                _type: "tooltip-trigger",
+                title,
+                content,
+              },
+            },
+            h("span", afterMatch),
+          ],
+        };
       }
     },
   };
